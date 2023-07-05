1c66acebd60567bf9a40b7e06b05a02c86a770d6
Sathish Periyasamy <S2605@SVSGLOBAL.COM>
2023-07-05 19:02:31 +0530

    working code

--- tests/light.test.ts
+++ tests/light.test.ts
@@ -1,34 +1,41 @@
-import { playAudit } from 'playwright-lighthouse'
-import { test,chromium } from '@playwright/test';
+import { playAudit } from 'playwright-lighthouse';
+import { test, chromium } from '@playwright/test';
+import { thresholds } from './threshold';
+// import { URLs } from './urls.json'
 
-test('audit example', async({playwright}) => {
+const options = {
+    loglevel: "info",
+}
+const URLs =[{
+        url:"https://uat.shriramfinance.in/"
+    },
+    {
+        url:"https://uat.shriramfinance.in/"
+      
+    }]
 
-    const browser = await playwright.chromium.launch({
-      args: ['--remote-debugging-port=9222'],
+URLs.forEach((data,idx) => {
+    test('Ligthouse performance test for '+(idx), async ({}) => {
+        const browser = await chromium.launch({
+            args: ['--remote-debugging-port=9222'],
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto(data.url);
+        await playAudit({
+            page: page,
+            thresholds: thresholds,
+            port: 9222,
+            opts: options,
+            reports: {
+                formats: {
+                    html: true, //defaults to false
+                },
+                name:"lighthouse-report",
+                directory:"lighthouse-report"+Date.now().toString()
+            },
+        });
+        await page.close();
+        await browser.close();
     })
-    const context=await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('https://shriramfinance.in/');
-
-    await playAudit({
-      thresholds:{
-        performance:50,
-        accessibility:50,
-        'best-practices':50,
-        seo:50,
-        pwa:50,
-      },
-      ignoreError:true,
-      page: page,
-      port: 9222,
-      reports:{
-        "formats":{html:true,csv:true,json:false},
-        name:"lighthouse-report",
-        directory:"lighthouse-report"+Date.now().toString()
-      }
-    });
-    await browser.close();
-    await page.close();
-    await context.close();
-
 });
---
+++ tests/threshold.ts
@@ -0,0 +1,6 @@
+export const thresholds = {
+    performance: 30,
+    accessibility: 30,
+    'best-practices': 30,
+    seo: 30
+}
